e5649e481df06d9560024c3bc45cdd21d5f24511
Sheilla-Chomba <chombasheila980@gmail.com>
2024-03-03 16:59:04 +0300

    Cart display

--- Frontend/Tuuze/src/app/components/navbar/navbar.component.ts
+++ Frontend/Tuuze/src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  showProductNav() {
+    let modalBg = document.querySelector('.prod-modal-bg-nav') as HTMLDivElement;
 
+    modalBg?.classList.add('modal-active-nav');
+  }
+  closeModalNav() {
+    let modalBg = document.querySelector('.prod-modal-bg-nav') as HTMLDivElement;
+
+    modalBg?.classList.remove('modal-active-nav');
+  }
 }
