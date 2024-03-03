5c365e834751bd478c183b93421ac709ca445bb9
Sheilla-Chomba <chombasheila980@gmail.com>
2024-03-03 22:25:25 +0300

    User dashboard completed

--- Frontend/Tuuze/src/app/components/user-dashboard/user-dashboard.component.ts
+++ Frontend/Tuuze/src/app/components/user-dashboard/user-dashboard.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, NavbarComponent, CommonModule],
   templateUrl: './user-dashboard.component.html',
-  styleUrl: './user-dashboard.component.css'
+  styleUrl: './user-dashboard.component.css',
 })
 export class UserDashboardComponent {
+  showProduct() {
+    let modalBg = document.querySelector('.prod-modal-bg') as HTMLDivElement;
 
+    modalBg?.classList.add('modal-active');
+  }
+  closeModal() {
+    let modalBg = document.querySelector('.prod-modal-bg') as HTMLDivElement;
+
+    modalBg?.classList.remove('modal-active');
+  }
 }
