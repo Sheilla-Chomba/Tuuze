7df7e4f81712c488eff3195363cf3017ce389ed1
Sheilla-Chomba <chombasheila980@gmail.com>
2024-03-03 17:49:58 +0300

    hamburger icon

--- Frontend/Tuuze/src/app/components/navbar/navbar.component.ts
+++ Frontend/Tuuze/src/app/components/navbar/navbar.component.ts
@@ -5,19 +5,33 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   showProductNav() {
-    let modalBg = document.querySelector('.prod-modal-bg-nav') as HTMLDivElement;
+    let modalBg = document.querySelector(
+      '.prod-modal-bg-nav'
+    ) as HTMLDivElement;
 
     modalBg?.classList.add('modal-active-nav');
   }
   closeModalNav() {
-    let modalBg = document.querySelector('.prod-modal-bg-nav') as HTMLDivElement;
+    let modalBg = document.querySelector(
+      '.prod-modal-bg-nav'
+    ) as HTMLDivElement;
 
     modalBg?.classList.remove('modal-active-nav');
+  }
+  showHamburgerMenu() {
+    let hamburgerList = document.querySelector('.mobile') as HTMLElement;
+
+    hamburgerList?.classList.add('mobile-menu-active');
+  }
+  closeHamburgerMenu() {
+    let hamburgerList = document.querySelector('.mobile') as HTMLElement;
+
+    hamburgerList?.classList.remove('mobile-menu-active');
   }
 }
