0a11fc565e97e0ca98692ca1f9c1273c39cccc2a
Sheilla-Chomba <chombasheila980@gmail.com>
2024-03-04 13:40:38 +0300

    nav bar  complete

--- Frontend/Tuuze/src/app/app.routes.ts
+++ Frontend/Tuuze/src/app/app.routes.ts
@@ -5,20 +5,21 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AdminCreateProductsComponent } from './components/admin-create-products/admin-create-products.component';
 import { AdminUpdateProductsComponent } from './components/admin-update-products/admin-update-products.component';
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
-import { AdminViewUsersComponent } from './components/admin-view-users/admin-view-users.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { AdminViewUsersComponent } from './components/admin-view-users/admin-view-users.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  {path:'admin',component:AdminDashboardComponent},
+  { path: 'admin', component: AdminDashboardComponent },
   { path: 'admin/create-products', component: AdminCreateProductsComponent },
   { path: 'admin/update-products', component: AdminUpdateProductsComponent },
   { path: 'admin/view-users', component: AdminViewUsersComponent },
-  { path: 'settings', component: ChangePasswordComponent },
-  {path:'user',component:UserDashboardComponent},
+  { path: 'admin/settings', component: ChangePasswordComponent },
+  { path: 'user', component: UserDashboardComponent },
+  { path: 'user/settings', component: ChangePasswordComponent },
   { path: '**', component: NotFoundComponent },
 ];
--- Frontend/Tuuze/src/app/components/navbar/navbar.component.ts
+++ Frontend/Tuuze/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +10,42 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isLoggedIn: boolean = false;
+  isNotLoggedIn:boolean=false;
+  admin: boolean = false;
+  user:boolean=false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isLoggedIn =
+          this.router.url !== '/admin' &&
+          this.router.url !== '/admin/create-products' &&
+          this.router.url !== '/admin/update-products' &&
+          this.router.url !== '/admin/view-users' &&
+          this.router.url !== '/settings' &&
+          this.router.url !== '/user';
+        this.isNotLoggedIn =
+          this.router.url !== '/' &&
+          this.router.url !== '/login' &&
+          this.router.url !== '/register' &&
+          this.router.url !== '/admin/settings' &&
+          this.router.url !== '/user/settings' &&
+          this.router.url !== '/**';
+        this.admin =
+          this.router.url !== '/admin' &&
+          this.router.url !== '/admin/create-products' &&
+          this.router.url !== '/admin/update-products' &&
+          this.router.url !== '/admin/settings' &&
+          this.router.url !== '/admin/view-users';
+        this.user =
+          this.router.url !== '/user' &&
+          this.router.url !== '/user/settings';
+      
+      }
+      
+    });
+  }
 
   showProductNav() {
     let modalBg = document.querySelector(
